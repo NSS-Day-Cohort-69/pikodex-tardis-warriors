28bbc0f62401283f68e608393ef046ece8fed8ec
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-01-31 14:01:25 -0600

    Added pseudocode algorithms

--- scripts/pikomon.js
+++ scripts/pikomon.js
@@ -2,3 +2,12 @@ import { getDatabase } from './database.js'
 
 const allPikomon = getDatabase()
 
+// Define and export a function to generate HTML, pikomonList
+
+    // Initialize empty String pikoHTML
+
+    // Loop all Pikomon
+
+        // Interpolate Pikomon object data into HTML String Template, adding to pikoHTML
+
+    // Return pikoHTML
