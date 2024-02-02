6bea5c64714d35cb79c1d27d686e3283d1baba03
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-02 13:47:21 -0600

    Modified piko-card layout, and made background white

--- scripts/pikomon.js
+++ scripts/pikomon.js
@@ -2,23 +2,25 @@ import { getDatabase } from './database.js'
 
 const allPikomon = getDatabase()
 
-// Define and export a function to generate HTML, pikomonList
+// Define and export a function to generate HTML, pikoList
 export const getPikoList = () => {
     let pikoHTML = ""
     for (const piko of allPikomon) {
         pikoHTML += `<section>
         <div class="piko-card">
-            <div class="piko-img">
-                <img src="${piko.imageUrl}">
-            </div>
-            <div class="piko-info">
-                <div class="piko-name">${piko.name}</div>
-                <div class="info-text">
-                    <ul>
-                        <li class="piko-category">Category: ${piko.category}</li>
-                        <li class="piko-abilities">Abilities: ${piko.abilities}</li>
-                        <li class="piko-weakness">Weakness: ${piko.weakness}</li>
-                    </ul>
+            <div class="piko-content">
+                <div class="piko-img">
+                    <img src="${piko.imageUrl}">
+                </div>
+                <div class="piko-info">
+                    <div class="piko-name">${piko.name}</div>
+                    <div class="info-text">
+                        <ul>
+                            <li class="piko-category">Category: ${piko.category}</li>
+                            <li class="piko-abilities">Abilities: ${piko.abilities}</li>
+                            <li class="piko-weakness">Weakness: ${piko.weakness}</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
